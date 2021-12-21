c9affd8132050e5d50b72cd73abb1ab153a2dddf
Marcus <marcuslt95@gmail.com>
2021-12-21 19:42:25 +0000

    responsive version of homepage mockup to give client hands on experience with the kind of aesthetic we can give them

--- in_development/static/assets/js/script.js
+++ in_development/static/assets/js/script.js
@@ -8,8 +8,6 @@ const mobileMenu = (function () {
     mobileMenu.classList.toggle("show-menu");
   };
 
-  mobileMenuBtn.addEventListener("click", showMenu);
-
   const menuAnimation = bodymovin.loadAnimation({
     // animationData: { /* ... */ },
     container: document.querySelector("#menu-container"), // required
@@ -20,16 +18,36 @@ const mobileMenu = (function () {
     name: "menuAnimation", // optional
   });
 
+  menuAnimation.goToAndStop(0, true);
+  menuAnimation.setSpeed(2);
+
   const animateMenu = function () {
     if (state === "closed") {
-      menuAnimation.playSegments([20, 90], true);
+      menuAnimation.playSegments([45, 72], true);
       state = "open";
+      showMenu();
     } else {
-      menuAnimation.playSegments([110, 180], true);
+      menuAnimation.playSegments([0, 30], true);
       state = "closed";
+      showMenu();
     }
   };
 
-  menuAnimation.goToAndStop(20, true);
   mobileMenuBtn.addEventListener("click", animateMenu);
+})();
+
+const header = (function () {
+  const header = document.querySelector("header");
+  const heroText = document.querySelector(".hero-text-wrapper");
+
+  const checkScroll = function () {
+    const currentScrollPos = window.pageYOffset;
+    if (currentScrollPos > 225) {
+      heroText.classList.add("hidden");
+    } else {
+      heroText.classList.remove("hidden");
+    }
+  };
+
+  window.addEventListener("scroll", checkScroll);
 })();

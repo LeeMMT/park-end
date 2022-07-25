dcde874c4149914032fbdacfad5c708450a4d799
Marcus <marcuslt95@gmail.com>
2022-07-25 18:39:19 +0100

    .

--- in_development/static/assets/js/header.js
+++ in_development/static/assets/js/header.js
@@ -1,85 +1,64 @@
 const mobileMenu = (function () {
-  const mobileMenuBtn = document.querySelector("#menu-container");
-  const mobileMenu = document.querySelector(".mobile-menu");
-  let state = closed;
+  const mobileMenuBtn = document.querySelector('#menu-container')
+  const mobileMenu = document.querySelector('.mobile-menu')
+  let state = closed
 
   const showMenu = function () {
-    document.body.classList.toggle("stop-scrolling");
-    mobileMenu.classList.toggle("show-menu");
-  };
-
-  const initialMenuPath = document.body.classList.contains("home") ? "menu.json" : "menu-black.json";
-
-  let menuAnimation = bodymovin.loadAnimation({
-    // animationData: { /* ... */ },
-    container: document.querySelector("#menu-container"), // required
-    path: `../static/assets/animations/${initialMenuPath}`, // required
-    renderer: "svg", // required
-    loop: false, // optional
-    autoplay: false, // optional
-    name: "menuAnimation", // optional
-  });
-
-  menuAnimation.goToAndStop(0, true);
-  menuAnimation.setSpeed(3);
-
-  function setMenuAnimation(filePath) {
-    document.querySelector("#menu-container").innerHTML = "";
-
-    menuAnimation = bodymovin.loadAnimation({
-      container: document.querySelector("#menu-container"),
-      path: `../static/assets/animations/${filePath}.json`,
-      renderer: "svg",
-      loop: false,
-      autoplay: false,
-      name: "menuAnimation",
-    });
-
-    menuAnimation.goToAndStop(0, true);
-    menuAnimation.setSpeed(3);
+    document.body.classList.toggle('stop-scrolling')
+    mobileMenu.classList.toggle('show-menu')
   }
 
+  const menuAnimation = bodymovin.loadAnimation({
+    container: document.querySelector('#menu-container'),
+    path: '../static/assets/animations/menu-black.json',
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    name: 'menuAnimation',
+  })
+
   const animateMenu = function () {
-    if (state === "closed") {
-      menuAnimation.playSegments([45, 72], true);
-      state = "open";
-      showMenu();
+    if (state === 'closed') {
+      menuAnimation.playSegments([45, 70], true)
+      state = 'open'
+      showMenu()
     } else {
-      menuAnimation.playSegments([0, 30], true);
-      state = "closed";
-      showMenu();
+      menuAnimation.playSegments([4, 30], true)
+      state = 'closed'
+      showMenu()
     }
-  };
+  }
 
-  mobileMenuBtn.addEventListener("click", animateMenu);
+  menuAnimation.goToAndStop(0, true)
+  menuAnimation.setSpeed(3)
 
-  return { setMenuAnimation };
-})();
+  mobileMenuBtn.addEventListener('click', animateMenu)
+})()
 
 const accordianModule = (function () {
-  const accordianitems = document.querySelectorAll(".accordian-item");
-  const arrowIcon = document.querySelector(".accordian .arrow-down");
+  const accordianitems = document.querySelectorAll('.accordian-item')
+  const arrowIcon = document.querySelector('.accordian .arrow-down')
 
   const resetSubItems = () => {
     accordianSubItems.forEach((el) => {
-      el.style.height = 0;
-      el.classList.remove("open");
-    });
-  };
+      el.style.height = 0
+      el.classList.remove('open')
+    })
+  }
 
   const toggleMenu = (e) => {
-    arrowIcon.classList.toggle("rotated");
-    if (e.target.nextElementSibling.classList.contains("open")) {
-      e.target.nextElementSibling.style.height = 0;
-      e.target.nextElementSibling.classList.toggle("open");
-      if (e.target.classList.contains("has-sub")) {
-        resetSubItems();
+    arrowIcon.classList.toggle('rotated')
+    if (e.target.nextElementSibling.classList.contains('open')) {
+      e.target.nextElementSibling.style.height = 0
+      e.target.nextElementSibling.classList.toggle('open')
+      if (e.target.classList.contains('has-sub')) {
+        resetSubItems()
       }
     } else {
-      e.target.nextElementSibling.style.height = `${e.target.nextElementSibling.scrollHeight}px`;
-      e.target.nextElementSibling.classList.toggle("open");
+      e.target.nextElementSibling.style.height = `${e.target.nextElementSibling.scrollHeight}px`
+      e.target.nextElementSibling.classList.toggle('open')
     }
-  };
+  }
 
-  accordianitems.forEach((el) => el.addEventListener("click", toggleMenu));
-})();
+  accordianitems.forEach((el) => el.addEventListener('click', toggleMenu))
+})()
--- in_development/static/assets/js/index.js
+++ in_development/static/assets/js/index.js
@@ -1,109 +1,23 @@
-const mobileMenu = (function () {
-  const mobileMenuBtn = document.querySelector("#menu-container");
-  const mobileMenu = document.querySelector(".mobile-menu");
-  let state = closed;
-
-  const showMenu = function () {
-    document.body.classList.toggle("stop-scrolling");
-    mobileMenu.classList.toggle("show-menu");
-  };
-
-  const initialMenuPath = document.body.classList.contains("home") ? "menu.json" : "menu-black.json";
-
-  let menuAnimation = bodymovin.loadAnimation({
-    // animationData: { /* ... */ },
-    container: document.querySelector("#menu-container"), // required
-    path: `../static/assets/animations/${initialMenuPath}`, // required
-    renderer: "svg", // required
-    loop: false, // optional
-    autoplay: false, // optional
-    name: "menuAnimation", // optional
-  });
-
-  menuAnimation.goToAndStop(0, true);
-  menuAnimation.setSpeed(3);
-
-  function setMenuAnimation(filePath) {
-    document.querySelector("#menu-container").innerHTML = "";
-
-    menuAnimation = bodymovin.loadAnimation({
-      container: document.querySelector("#menu-container"),
-      path: `../static/assets/animations/${filePath}.json`,
-      renderer: "svg",
-      loop: false,
-      autoplay: false,
-      name: "menuAnimation",
-    });
-
-    menuAnimation.goToAndStop(0, true);
-    menuAnimation.setSpeed(3);
-  }
-
-  const animateMenu = function () {
-    if (state === "closed") {
-      menuAnimation.playSegments([45, 72], true);
-      state = "open";
-      showMenu();
-    } else {
-      menuAnimation.playSegments([0, 30], true);
-      state = "closed";
-      showMenu();
-    }
-  };
-
-  mobileMenuBtn.addEventListener("click", animateMenu);
-
-  return { setMenuAnimation };
-})();
-
-const accordianModule = (function () {
-  const accordianitems = document.querySelectorAll(".accordian-item");
-  const arrowIcon = document.querySelector(".accordian .arrow-down");
-
-  const resetSubItems = () => {
-    accordianSubItems.forEach((el) => {
-      el.style.height = 0;
-      el.classList.remove("open");
-    });
-  };
-
-  const toggleMenu = (e) => {
-    arrowIcon.classList.toggle("rotated");
-    if (e.target.nextElementSibling.classList.contains("open")) {
-      e.target.nextElementSibling.style.height = 0;
-      e.target.nextElementSibling.classList.toggle("open");
-      if (e.target.classList.contains("has-sub")) {
-        resetSubItems();
-      }
-    } else {
-      e.target.nextElementSibling.style.height = `${e.target.nextElementSibling.scrollHeight}px`;
-      e.target.nextElementSibling.classList.toggle("open");
-    }
-  };
-
-  accordianitems.forEach((el) => el.addEventListener("click", toggleMenu));
-})();
-
 const multiImageComponent = (function () {
-  const multiImageSection = document.querySelector(".multi-image");
+  const multiImageSection = document.querySelector('.multi-image')
   const changeBgImage = function (e) {
-    const dataI = e.target.getAttribute("data-i");
+    const dataI = e.target.getAttribute('data-i')
     switch (+dataI) {
       case 1:
-        multiImageSection.style.backgroundImage = "url(../static/assets/images/animals/cattle.jpeg)";
-        break;
+        multiImageSection.style.backgroundImage = 'url(../static/assets/images/animals/cattle.jpeg)'
+        break
       case 2:
-        multiImageSection.style.backgroundImage = "url(../static/assets/images/animals/piglet.jpeg)";
-        break;
+        multiImageSection.style.backgroundImage = 'url(../static/assets/images/animals/piglet.jpeg)'
+        break
       case 3:
-        multiImageSection.style.backgroundImage = "url(../static/assets/images/animals/cattle.jpeg)";
-        break;
+        multiImageSection.style.backgroundImage = 'url(../static/assets/images/animals/cattle.jpeg)'
+        break
       case 4:
-        multiImageSection.style.backgroundImage = "url(../static/assets/images/animals/piggies.jpeg)";
-        break;
+        multiImageSection.style.backgroundImage = 'url(../static/assets/images/animals/piggies.jpeg)'
+        break
     }
-  };
-  document.querySelectorAll(".multi-image a").forEach((el) => {
-    el.addEventListener("mouseover", changeBgImage);
-  });
-})();
+  }
+  document.querySelectorAll('.multi-image a').forEach((el) => {
+    el.addEventListener('mouseover', changeBgImage)
+  })
+})()
